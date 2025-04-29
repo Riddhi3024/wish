d39dc5d18365d512fe21d7ba02970d7e67b14bec
Riddhi3024 <riddhii470@gmail.com>
2025-04-29 15:30:35 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,5 @@
 <?php
-// Check if we're in GitHub Actions environment
-$host = getenv('GITHUB_ACTIONS') ? 'mysql' : 'localhost';  // Default to 'mysql' in GitHub Actions, 'localhost' locally
-
-$conn = mysqli_connect($host, "u800183464_familyhub", "Nf1US9:b*", "u800183464_familyhub");
+$conn = mysqli_connect("mysql", "root", "", "famillytree");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
