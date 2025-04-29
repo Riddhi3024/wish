695a01eace236a2553305e25fba4c0ac77349bf9
Riddhi3024 <riddhii470@gmail.com>
2025-04-29 15:34:56 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("mysql", "root", "", "famillytree");
+$conn = mysqli_connect("mysql", "root", "rootpassword", "familytree");  // Use 'mysql' as the host in GitHub Actions
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
