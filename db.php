d8325f94b75e18a0a82e714961f799ba92986735
Riddhi3024 <riddhii470@gmail.com>
2025-04-29 15:42:31 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,5 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "rootpassword", "familytree");
-
-
+$conn = mysqli_connect("127.0.0.1", "root", "rootpassword", "familytree", 3306);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
